01bd7b1c2a134a6195ba276f073e161ecce8e598
zombonline <megazombie130@gmail.com>
2025-11-19 17:50:34 +0000

    fixed password stored in sql test data script

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -5,4 +5,4 @@ USE berties_books;
 INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99) ;
 
 INSERT INTO users (userName, hashed_password, first_name, last_name, email)VALUES
-('gold', '$2b$10$7.F9mqAo8EAc876E2d0cpeWFu0tGxPr5eZCnhzpw6oI2JpT3H9aKu', 'John', 'Goldmiths', 'john_goldsmiths@example.com');
+('gold', '$2b$10$rORyNzCI4ILqsZS2JFTsGezKwqSbi2.lhseR6pIZvCdZPGOA2o/aS', 'John', 'Goldmiths', 'john_goldsmiths@example.com');
