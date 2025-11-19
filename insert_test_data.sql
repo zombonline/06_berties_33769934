3aab39effffdf4894a230f271096d7dadf69841f
zombonline <megazombie130@gmail.com>
2025-11-19 17:00:27 +0000

    Updated test data sql script for lab 7

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -3,3 +3,6 @@
 USE berties_books;
 
 INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99) ;
+
+INSERT INTO users (userName, hashed_password, first_name, last_name, email)VALUES
+('gold', '$2b$10$7.F9mqAo8EAc876E2d0cpeWFu0tGxPr5eZCnhzpw6oI2JpT3H9aKu', 'John', 'Goldmiths', 'john_goldsmiths@example.com');
